74bdc30a98989800b043aba7fc8a569cf615abf1
GwonHJ <45057466+GwonHJ@users.noreply.github.com>
2020-11-07 16:43:41 +0900

    Rename token_Ex2.py to Ex/token_Ex2.py

--- token_Ex2.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  7 02:23:56 2020
-
-@author: 현
-"""
-from nltk.tokenize import WordPunctTokenizer
-print(WordPunctTokenizer().tokenize("Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery goes for a pastry shop."))
