58d3500086fba1b5401d3c6e9f08120792aa4bec
GwonHJ <45057466+GwonHJ@users.noreply.github.com>
2020-11-07 16:43:21 +0900

    Rename token_Ex.py to Ex/token_Ex.py

--- token_Ex.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  7 00:51:17 2020
-
-@author: 현
-"""
-from nltk.tokenize import word_tokenize  
-print(word_tokenize("Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery as cheery goes for a pastry shop."))  
